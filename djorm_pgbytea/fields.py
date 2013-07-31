1cab46485ff2f7b92ac1458f73994c7527f7f899
carymrobbins <carymrobbins@gmail.com>
2013-07-31 13:08:00 -0500

    Added south support for ByteaField.

--- djorm_pgbytea/fields.py
+++ djorm_pgbytea/fields.py
@@ -4,3 +4,10 @@ from .bytea import ByteaField
 from .lobject import LargeObjectFile, LargeObjectField
 
 __all__ = ['ByteaField', 'LargeObjectField', 'LargeObjectFile']
+
+# South support
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ['^djorm_pgbytea\.fields\.ByteaField'])
+except ImportError:
+    pass
