bb0fcaf5f087ee2bb49a27451473e08ba575cae6
Andrey Antukh <niwi@niwi.be>
2014-09-13 10:31:56 +0200

    Minor changes on setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ Core module of django orm extensions package. Is a collection of third party plu
 
 setup(
     name = "djorm-ext-pgbytea",
-    version = '0.6.0',
-    url = 'https://github.com/niwibe/djorm-ext-pgbytea',
-    license = 'BSD',
-    platforms = ['OS Independent'],
+    version = "0.6.0",
+    url = "https://github.com/niwibe/djorm-ext-pgbytea",
+    license = "BSD",
+    platforms = ["OS Independent"],
     description = description.strip(),
-    author = 'Andrey Antukh',
-    author_email = 'niwi@niwi.be',
-    maintainer = 'Andrey Antukh',
-    maintainer_email = 'niwi@niwi.be',
+    author = "Andrey Antukh",
+    author_email = "niwi@niwi.be",
+    maintainer = "Andrey Antukh",
+    maintainer_email = "niwi@niwi.be",
     packages = find_packages(),
     include_package_data = False,
     install_requires = [
@@ -22,12 +22,12 @@ setup(
     ],
     zip_safe = False,
     classifiers = [
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
     ]
 )
